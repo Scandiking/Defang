c547c97ed34106125fd3e0b4d8adf286355ee56c
Scandiking <57848767+Scandiking@users.noreply.github.com>
2026-08-21 23:26:47 +0200

    chore: bump version to 0.1.16 (versionCode 17)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.defang.launcher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.1.15"
+        versionCode = 17
+        versionName = "0.1.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
